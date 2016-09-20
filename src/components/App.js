66a48c05972db315f5137dda0a75ad71f693d656
omar.chacin <omar.chacin@sigmagroup.no>
2016-09-20 13:56:31 +0200

    Edit button functionality added

--- src/components/App.js
+++ src/components/App.js
@@ -29,13 +29,17 @@ class App extends React.Component {
       <div>
         <h1>Todos App</h1>
         <CreateTodo createTask={this.createTask.bind(this)}/>
-        <TodosLists todos={this.state.todos} toggleTask={this.toggleTask.bind(this)}/>
+        <TodosLists
+          todos={this.state.todos}
+          toggleTask={this.toggleTask.bind(this)}
+          saveTask={this.saveTask.bind(this)}
+        />
       </div>
     );
   }
 
   toggleTask(task) {
-    const foundTodo = _.find(this.state.todos, todo => todo.task === task);
+    const foundTodo       = _.find(this.state.todos, todo => todo.task === task);
     foundTodo.isCompleted = !foundTodo.isCompleted;
     this.setState({
       todos: this.state.todos
@@ -45,11 +49,19 @@ class App extends React.Component {
   createTask(task) {
     console.log('hi task ', task);
     this.state.todos.push({
-      task: task,
-      isCompleted:false
+      task       : task,
+      isCompleted: false
     });
     this.setState({
       todo: this.state.todos
+    })
+  }
+
+  saveTask(oldTask, newTask) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task = oldTask);
+    foundTodo.task = newTask;
+    this.setState({
+      todos: this.state.todos
     })
   }
 }
--- src/components/TodosListItem.js
+++ src/components/TodosListItem.js
@@ -18,6 +18,16 @@ class TodosListItem extends React.Component {
       cursor: 'pointer'
     };
 
+    if(this.state.isEditing) {
+      return (
+        <td>
+          <form onSubmit={this.onSaveClick.bind(this)}>
+            <input type="text" defaultValue={task} ref="editInput"/>
+          </form>
+        </td>
+      )
+    }
+
     return (
       <td style={taskStyle}
           onClick={this.props.toggleTask.bind(this, task)}>
@@ -30,7 +40,7 @@ class TodosListItem extends React.Component {
     if (this.state.isEditing) {
       return (
         <td>
-          <button>Save</button>
+          <button onClick={this.onSaveClick.bind(this)}>Save</button>
           <button onClick={this.onCancelClick.bind(this)}>Cancel</button>
         </td>
       );
@@ -61,6 +71,17 @@ class TodosListItem extends React.Component {
   }
 
   onCancelClick() {
+    this.setState({
+      isEditing: false
+    })
+  }
+
+  onSaveClick(event) {
+    event.preventDefault();
+
+    const oldTask = this.props.task;
+    const newTask = this.refs.editInput.value;
+    this.props.saveTask(oldTask, newTask);
     this.setState({
       isEditing: false
     })
